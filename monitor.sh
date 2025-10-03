52fbbc533583d815e5c7abb8021a47297973a9b3
AlexiaCordeiro <alexiacordeiro@tutanota.com>
2025-10-03 11:37:21 -0300

    Automation for open and closed lid

--- monitor.sh
+++ monitor.sh
@@ -5,6 +5,21 @@ set -e
 connected_count=$(xrandr | grep -c " connected ")
 primary_display=$(xrandr | grep -E "eDP[^ ]* connected " | awk '{print $1}')
 secondary_display=$(xrandr | grep -E "HDMI[^ ]* connected " | awk '{print $1}')
+lid_state=$(cat /proc/acpi/button/lid/LID/state | grep -oE "open|closed" | head -1)
+
+
+lid_open=false
+
+if [ "$lid_state" = "open" ]; then
+    bspc monitor "${primary_display}" -d I II III IV V
+    bspc monitor "${secondary_display}" -d VI VII VIII IX X
+    bspc monitor "${secondary_display}" -s "${primary_display}"
+    bspc wm --reorder-monitors "${secondary_display}" "${primary_display}"
+else
+    bspc monitor "${secondary_display}" -d I II III IV V
+    bspc monitor "${primary_display}" --remove
+    bspc monitor "${secondary_display}" -f
+fi
 
 if [ "$connected_count" -gt 1 ]; then
   if ! xrandr --output ${secondary_display} --primary --mode 1920x1080 --output ${primary_display} --mode 1920x1080 --left-of ${secondary_display} 2>/dev/null; then
